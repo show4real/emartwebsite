75273536529d126cd1902c42636758558d1f3003
Showole Oladayo <show4ril@gmail.com>
2023-07-28 20:06:44 +0100

    changes

--- resources/views/vendor_include/init_firebase.blade.php
+++ resources/views/vendor_include/init_firebase.blade.php
@@ -1,15 +1,13 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-var firebaseConfig = {
-apiKey: "AIzaSyDo6-cOq23v15hpKn57onnfmEZ-2Lg_87s",
-authDomain: "emartweb-86a28.firebaseapp.com",
-databaseURL: "https://emartweb-86a28-default-rtdb.firebaseio.com",
-projectId: "emartweb-86a28",
-storageBucket: "emartweb-86a28.appspot.com",
-messagingSenderId: "452879535542",
-appId: "1:452879535542:web:a4c055a9f2a99ee3c84517",
-measurementId: "G-VXJ3J6TW2W"
+const firebaseConfig = {
+apiKey: "AIzaSyDQrJSBDjSkF6HmUYDxv6pYbtYoNl5MUd8",
+authDomain: "laundrybasket-f3247.firebaseapp.com",
+projectId: "laundrybasket-f3247",
+storageBucket: "laundrybasket-f3247.appspot.com",
+messagingSenderId: "320755508506",
+appId: "1:320755508506:web:19e492ef47c6b8ed2cb8a3",
+measurementId: "G-V1PEGWTNEQ"
 };
-
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
